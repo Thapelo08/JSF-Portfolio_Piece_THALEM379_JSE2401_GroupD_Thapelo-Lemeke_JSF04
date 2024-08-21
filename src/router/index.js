9b374400f1f6507d809130f844f0583e83a6496f
Thapelo Lemeke <thapelolemeke15@gmail.com>
2024-08-21 09:20:08 +0200

    Documented the index.js file using jsDoc

--- src/router/index.js
+++ src/router/index.js
@@ -6,6 +6,10 @@ import ComparisonList from '../views/ComparisonList.vue';
 import Wishlist from '../components/Wishlist.vue';
 import WishlistButton from '../components/WishlistButton.vue';
 
+/**
+ * Array of route definitions for the Vue Router.
+ * @constant {Array<Object>} routes
+ */
 const routes = [
   {
     path: '/login',
@@ -28,7 +32,7 @@ const routes = [
   {
     path: '/wishlist',
     name: 'Wishlist',
-    component: Wishlist, 
+    component: Wishlist,
     meta: { requiresAuth: true },
   },
   {
@@ -47,15 +51,27 @@ const routes = [
     path: '/WishlistButton',
     name: 'WishlistButton',
     component: WishlistButton,
-    meta: { requiresAuth: true},
+    meta: { requiresAuth: true },
   },
 ];
 
+/**
+ * Vue Router instance configured with the history mode and routes.
+ * @constant {Object} router
+ */
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes,
 });
 
+/**
+ * Global navigation guard to check authentication before navigating to protected routes.
+ * Redirects to the login page if the user is not authenticated and tries to access a protected route.
+ * @function
+ * @param {Object} to - The target Route Object being navigated to.
+ * @param {Object} from - The current Route Object being navigated away from.
+ * @param {Function} next - A function that must be called to resolve the hook.
+ */
 router.beforeEach((to, from, next) => {
   const isAuthenticated = !!localStorage.getItem('token');
   
